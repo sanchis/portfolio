03bc7e0439edce06da006e48b6b763739d50fe7f
sanchis <sanchis1992@gmail.com>
2021-07-12 15:14:41 +0200

    feat: add buefy

--- components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section class="section">
-    <h1 class="title is-1">Hola! Soy <strong>Adrián Sanchís Gallego</strong></h1>
-  </section>
-</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,13 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'balm-ui/dist/balm-ui.css',
     '~/assets/styles/global.css'
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/balm-ui'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -39,6 +33,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    'nuxt-buefy'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
