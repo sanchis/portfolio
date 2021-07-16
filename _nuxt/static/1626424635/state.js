516661f5b7441cbbf99b2b9fd1d42ef01709a9af
sanchis <sanchis1992@gmail.com>
2021-07-16 09:59:40 +0000

    Deploying to gh-pages from  @ 29636d4d39031ee7f2c59e8321f8ecadea251838 🚀

--- _nuxt/static/1626424635/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1626424635",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"439aa3c7"}}}}(null));
