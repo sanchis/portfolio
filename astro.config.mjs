207a2d588943bfb040844fb34b02390bc41dcef3
Adrian Sanchis Gallego <adrian@devaway.es>
2021-12-31 12:21:59 +0100

    finish migration astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,4 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
-  renderers: ['@astrojs/renderer-preact'],
 });
